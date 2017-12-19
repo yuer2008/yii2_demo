01a2cdf5f51581a430ae5d7104ff564ef42df0f3
yuer2008 <yangfutao2000@163.com>
2017-12-19 17:56:21 +0800

    same last

--- console/config/main.php
+++ console/config/main.php
@@ -1,12 +1,12 @@
 <?php
 $params = array_merge(
     require __DIR__ . '/../../common/config/params.php',
-    require __DIR__ . '/../../common/config/params-local.php',
-    require __DIR__ . '/params.php',
-    require __DIR__ . '/params-local.php'
+    // require __DIR__ . '/../../common/config/params-local.php',
+    require __DIR__ . '/params.php'
+    // require __DIR__ . '/params-local.php'
 );
 
-return [
+$config = [
     'id' => 'app-console',
     'basePath' => dirname(__DIR__),
     'bootstrap' => ['log'],
@@ -30,6 +30,9 @@ return [
                 ],
             ],
         ],
+        
     ],
     'params' => $params,
 ];
+
+return $config;
